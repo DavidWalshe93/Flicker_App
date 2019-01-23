1c6908f08b3dc1ba900ebff5d9ec1d16e3dd6aaf
David Walshe <david.walshe93@gmail.com>
2019-01-23 21:36:31 +0000

    - Sourced From basic activity sample layout - Removed Floating button - Adding Async task for downloading Data

---
+++ app/src/main/java/com/example/david/flickr_viewer_app/GetRawData.java
@@ -0,0 +1,13 @@
+package com.example.david.flickr_viewer_app;
+
+import android.os.AsyncTask;
+
+//David Walshe
+//23/01/2019
+
+enum DownloadStatus { IDLE, PROCESSING, NOT_INTIALISED, FAILED_OR_EMPTY, OK }
+
+class GetRawData extends AsyncTask<String, Void, String> {
+
+
+}
